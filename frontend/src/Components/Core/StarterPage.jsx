315db79cfd6463c10c675bae69878d19611108b2
Mirza Saeed <89970740+MirzaSaeed@users.noreply.github.com>
2023-07-23 01:05:31 +0500

    bug fixes

--- frontend/src/Components/Core/StarterPage.jsx
+++ frontend/src/Components/Core/StarterPage.jsx
@@ -83,7 +83,7 @@ const StarterPage = () => {
     // ! Fetch API data
     let response = await axios
       .post(`${BASE_URL}/auth/employee/login/`, formData)
-      .then.catch((Error) => alert(JSON.stringify(Error.response.data)));
+      .catch((Error) => alert(JSON.stringify(Error.response.data)));
     if (response.data) {
       localStorage.setItem("user", JSON.stringify(response.data));
       navigate("/auth/employee/home");
